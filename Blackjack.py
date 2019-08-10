cfa554c64302e281d5f35b08a4b46dc03cb40e3e
Alexander Large <alexanderklarge@gmail.com>
2019-08-10 23:46:41 +0100

    Adding my Blackjack file to the repo

---
+++ Blackjack.py
@@ -0,0 +1,12 @@
+#My blackjack code!
+
+#Probably the best way to do it is to have a dictionary from 1 - 14
+#with 11 being ace, 12 being Jack, 13 being Queen, 14 being King
+
+#TO DO:
+    # Give player two cards
+    # Have 4 of each card, so if player or dealer gets one, they have less chance of getting same
+    # Have "hit/stick" ability 
+    
+#LATER 
+    # Have ability to bet!
